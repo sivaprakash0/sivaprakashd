98058639c5ff03f5628ec8b972f0d270adfb8aad
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-23 17:12:54 +0530

    Update sumall.java

--- sumall.java
+++ sumall.java
@@ -1,18 +1,15 @@
-package set1;
-
 import java.util.Scanner;
-
 public class sumall {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
 	int sz=in.nextInt();
 	int arr[]=new int[sz];
 	for(int i=0;i<sz;i++){
-		arr[i]=in.nextInt();
+	   arr[i]=in.nextInt();
 	}
 	int tot=0;
 	for(int j=0;j<sz;j++){
-	tot=arr[j]+tot;	
+	   tot=arr[j]+tot;	
 	}
 	System.out.println(tot);
 }
