f5a040caa365f47ede72ab3f4254f2143fba8ac2
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-24 18:11:29 +0530

    Update posbdivarr.java

--- posbdivarr.java
+++ posbdivarr.java
@@ -1,7 +1,4 @@
-package set1;
-
 import java.util.Scanner;
-
 public class posbdivarr {
 public static void main(String[] args) {
 	Scanner rd=new Scanner(System.in);
@@ -11,28 +8,25 @@ public static void main(String[] args) {
 		ar[i]=rd.nextInt();
 	}
 	if(sz>2){
-	int len=ar.length;
-	int fhl=len/2;
-	int sum=0;
-	
-	for(int j=0;j<fhl;j++){
+	  int len=ar.length;
+	  int fhl=len/2;
+	  int sum=0;
+	    for(int j=0;j<fhl;j++){
 		sum=sum+ar[j];
-	}
-	int fha=sum/(fhl);
-	
-	int tot=0;
-	for(int k=fhl;k<len;k++){
+	    }
+	  int fha=sum/(fhl);
+	  int tot=0;
+	    for(int k=fhl;k<len;k++){
 		tot=tot+ar[k];
-	}
-	int shl=ar.length-fhl;
-	
-	int sha=(tot/shl);
-	if(fha==sha){
-	System.out.println("yes");
-	}
-	else{
+	     }
+	  int shl=ar.length-fhl;
+	  int sha=(tot/shl);
+	     if(fha==sha){
+	        System.out.println("yes");
+	     }
+	     else{
 		System.out.println("no");
-	}
+	     }
 	}
 	else{
 		System.out.println("no");
