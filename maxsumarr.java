4c62f206e8c0a8177adad5c650d2958c2ca7b7b8
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-27 18:02:40 +0530

    Add files via upload

---
+++ maxsumarr.java
@@ -0,0 +1,33 @@
+package set1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class maxsumarr {
+public static void main(String[] args) {
+	Scanner rd=new Scanner(System.in);
+	int sz=rd.nextInt();
+	int ar[]=new int[sz];
+	for(int i=0;i<sz;i++){
+		ar[i]=rd.nextInt();
+	}
+	Arrays.sort(ar);
+	int l=ar.length;
+	int sum=0;
+	if(ar[l-1]<=9 && ar[l-2]<=9){
+		sum=ar[l-1]+ar[l-2];
+		System.out.println(sum);
+	}
+	if(ar[l-1]>9){
+		int a=ar[l-1]%10;
+		int b=ar[l-1]/10;
+		if(a>b){
+			sum=a+ar[l-2];
+		System.out.println(sum);}
+		else{
+			sum=b+ar[l-2];
+		System.out.println(sum);}
+	}
+	
+}
+}
