256e2887843db98407c0e9a2ba77196453cf0409
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-31 14:37:35 +0530

    Add files via upload

---
+++ swap2no.java
@@ -0,0 +1,17 @@
+package set4;
+
+import java.util.Scanner;
+
+public class swap2no {
+public static void main(String[] args) {
+  Scanner in=new Scanner(System.in);
+  int a=in.nextInt();
+  int b=in.nextInt();
+  int c=0;
+  c=a;
+  a=b;
+  b=c;
+  System.out.println(a+" "+b);
+  
+ }
+}
