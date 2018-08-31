f3886ccf71b419932d8455aad423d26bfd861998
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-31 14:41:40 +0530

    Add files via upload

---
+++ maxof10no.java
@@ -0,0 +1,16 @@
+package set4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class maxof10no {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int ar[]=new int[10];
+	for(int i=0;i<ar.length;i++){
+		ar[i]=in.nextInt();
+	}
+	Arrays.sort(ar);
+	System.out.println(ar[9]);
+}
+}
