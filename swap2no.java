5c1d084ea1aa0ad8215a43ae69e1601014f7a62d
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-31 14:38:01 +0530

    Update swap2no.java

--- swap2no.java
+++ swap2no.java
@@ -1,7 +1,4 @@
-package set4;
-
 import java.util.Scanner;
-
 public class swap2no {
 public static void main(String[] args) {
   Scanner in=new Scanner(System.in);
@@ -12,6 +9,5 @@ public static void main(String[] args) {
   a=b;
   b=c;
   System.out.println(a+" "+b);
-  
  }
 }
